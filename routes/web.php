d55f1549d5c1c33091ff8d5f0faa107e3f786d3f
Isaac <isaac.rojas@usach.cl>
2021-12-21 09:34:17 -0300

    [Isaac Rojas] - Vistas

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});
+*/
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/ingresarcurso', function () {
+    return view('ingresarcurso');
 });
 
 //Subjects
